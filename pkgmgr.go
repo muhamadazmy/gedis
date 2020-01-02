e4fbf54ab716213817bca9a5eebf04d6322b3738
Muhamad Azamy <muhamad@incubaid.com>
2020-01-02 16:05:36 +0100

    add missing file, and rename transort

---
+++ pkgmgr.go
@@ -0,0 +1,88 @@
+package gedis
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"sync"
+)
+
+// PackageManager interface
+type PackageManager interface {
+	// Call a package function with given arguments
+	Call(pkg, fn string, args ...interface{}) (Results, error)
+	// Add loads and register a new package with given name and code directory
+	Add(name, path string) error
+	// Remove a package from manager, the package won't be callable afterwards
+	Remove(name string) error
+	// List available package names
+	List() []string
+}
+
+// luaPackageManager manage package loading/unloading
+type luaPackageManager struct {
+	packages map[string]Package
+	modules  []Module
+	m        sync.RWMutex
+}
+
+// NewPackageManager creates a new package manager
+func NewPackageManager(modules ...Module) PackageManager {
+	return &luaPackageManager{
+		packages: make(map[string]Package),
+		modules:  modules,
+	}
+}
+
+func (m *luaPackageManager) Call(pkg, fn string, args ...interface{}) (Results, error) {
+	m.m.RLock()
+	defer m.m.RUnlock()
+
+	p, ok := m.packages[pkg]
+	if !ok {
+		return nil, fmt.Errorf("unknown package '%s'", pkg)
+	}
+
+	return p.Call(fn, args...)
+}
+
+func (m *luaPackageManager) Add(name, path string) error {
+	m.m.Lock()
+	defer m.m.Unlock()
+
+	pkg, err := NewPackage(path, m.modules...)
+	if err != nil {
+		return errors.Wrapf(err, "failed to add package: %s(%s)", name, path)
+	}
+
+	m.packages[name] = pkg
+	return nil
+}
+
+func (m *luaPackageManager) Remove(name string) error {
+	m.m.Lock()
+	defer m.m.Unlock()
+
+	pkg, ok := m.packages[name]
+	if !ok {
+		return nil
+	}
+
+	if err := pkg.Close(); err != nil {
+		return err
+	}
+
+	delete(m.packages, name)
+	return nil
+}
+
+func (m *luaPackageManager) List() []string {
+	m.m.RLock()
+	defer m.m.RUnlock()
+
+	var pkgs []string
+	for k := range m.packages {
+		pkgs = append(pkgs, k)
+	}
+
+	return pkgs
+}
